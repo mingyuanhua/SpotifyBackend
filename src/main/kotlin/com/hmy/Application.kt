4e4ac388d5f8f090ecc6bcb422024baac5209a74
mingyuanhua <m.y.hua@hotmail.com>
2023-07-16 14:40:54 -0700

    init commit

---
+++ src/main/kotlin/com/hmy/Application.kt
@@ -0,0 +1,29 @@
+package com.hmy
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+        })
+    }
+    // TODO: adding the routing configuration here
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
